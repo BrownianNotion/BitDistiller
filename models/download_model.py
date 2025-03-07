abe73dfd7131783dcce34d1c030fca1bdd401e36
Alex Coleman <111077126+alexncoleman@users.noreply.github.com>
2025-03-07 09:55:36 +0000

    fixing download model

--- models/download_model.py
+++ models/download_model.py
@@ -1,4 +1,4 @@
-from huggingface_hub import snapshot_download
+from huggingface_hub import hf_hub_download
 
 
 # Model repo on Hugging Face
@@ -6,6 +6,6 @@ model_name = "QuantFactory/TinyLlama_v1.1-GGUF"
 filename = "TinyLlama_v1.1.Q2_K.gguf"
 
 # Download the entire model into the current directory
-snapshot_download(repo_id=model_name, filename=filename, local_dir="models/TinyLlama_v1.1-GGUF")
+hf_hub_download(repo_id=model_name, filename=filename, local_dir="models/TinyLlama_v1.1-GGUF")
 
 print(f"Model {model_name} downloaded to models/TinyLlama_v1.1")
