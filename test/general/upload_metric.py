9a59d45083b1f5bd8e6afba70c55d47b617307ad
BrownianNotion <andrewwusyd@gmail.com>
2025-03-17 23:12:00 +0000

    Working draft of metrics table

--- test/general/upload_metric.py
+++ test/general/upload_metric.py
@@ -1,48 +1,56 @@
-from pytablewriter import LatexTableWriter
+from pytablewriter import MarkdownTableWriter
 import json
 import argparse
 import pathlib
 
-from huggingface_hub import HfApi
+from huggingface_hub import HfApi, hf_hub_download 
+from huggingface_hub.utils import EntryNotFoundError
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--metrics_file", type=str, help="json file with model metrics")
-parser.add_argument("--model_repo", type=str, "model repo to upload metric to")
+parser.add_argument("--model_repo", type=str, help="model repo to upload metric to")
 
 args = parser.parse_args()
 
-table = LatexTableWriter()
-table.headers = ["PPL", "arc_easy", "arc_challenge", "piqa", "winogrande", "hellaswag", "mmlu"]
+table_writer = MarkdownTableWriter()
+table_writer.headers = ["PPL", "arc_easy", "arc_challenge", "piqa", "winogrande", "hellaswag", "mmlu"]
 
-results_json = json.load(args.metrics_file)
+with open(args.metrics_file, "r") as f:
+    results_json = json.load(f)
+
 results = []
-for metric in table.headers:
+for metric in table_writer.headers:
     if metric in results_json:
-        if "acc" in results_json[metric]:
-            acc, std = results_json[metric]['acc'], results_json[metric]['std']
+        # qa metrics
+        if isinstance(results_json[metric], dict):
+            acc, std = results_json[metric]['acc'], results_json[metric]['acc_stderr']
             acc *= 100
             std *= 100
-
-            results.append(f"{acc:.2f}+-{std:.2f}")
+            results.append(f"{acc:.2f} ± {std:.2f}")
+        # ppl/mmlu
         else:
-            results.append(f"{100 * results_json[metric]:.2f}")
+            scale_factor = 1 if metric == "PPL" else 100
+            results.append(f"{scale_factor * results_json[metric]:.2f}")
     else:
-        results.append("")
+        # blank (eg. mmlu/hellaswag may be blank because they take 
+        # long to run and we can run later)
+        results.append("-")
 
-table.value_matrix = [results]
+table_writer.value_matrix = [results]
 
 modelcard_path = pathlib.Path(args.metrics_file).parent / "README.md"
-with open(modelcard_path, "w+ ") as f:
+
+with open(modelcard_path, "w+") as f:
     f.write(
         "**Metrics Table**:\n"
-        "$$\n"
-        f"{table.write_table()}"
-        "$$\n"
         )
+    table_writer.stream = f
+    table_writer.write_table()
 
 api = HfApi()
 api.upload_file(
     path_or_fileobj=modelcard_path,
     path_in_repo="README.md",
-    repo_id=args.model_repo
+    repo_id=args.model_repo,
+    commit_message="Upload model metrics"
 )
