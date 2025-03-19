e90c9e07cde5fe9b62a07856da1614640783a61e
victorfiz <victorfizesan@gmail.com>
2025-03-19 17:06:55 +0000

    more path changing tinyllama_v1.1 to Llama-2-7b-hf

--- models/download_model.py
+++ models/download_model.py
@@ -1,10 +1,9 @@
 from huggingface_hub import snapshot_download
-import os
 
 # Model repo on Hugging Face
-model_name = "TinyLlama/TinyLlama_v1.1"
+model_name = "meta-llama/Llama-2-7b-hf"
 
 # Download the entire model into the current directory
-snapshot_download(repo_id=model_name, local_dir="models/TinyLlama_v1.1")
+snapshot_download(repo_id=model_name, local_dir="models/Llama-2-7b-hf")
 
-print(f"Model {model_name} downloaded to models/TinyLlama_v1.1")
+print(f"Model {model_name} downloaded to models/Llama-2-7b-hf")
