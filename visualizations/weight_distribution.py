bef4965d25e1f0f5b6a443c333d2e6ef44e1f02d
heisenger <julianma02@gmail.com>
2025-03-19 00:17:44 +0000

    updated plot names

--- visualizations/weight_distribution.py
+++ visualizations/weight_distribution.py
@@ -73,5 +73,5 @@ def main():
 
 if __name__ == "__main__":
     # Example usage:
-    # python weight_distribution.py --file_path ../quantization/clip_cache/TinyLlama_v1.1/int2-g128.pt --plot_name tiny_llama1.1
+    # python weight_distribution.py --file_path ../quantization/clip_cache/TinyLlama_v1.1/int2-g128.pt --plot_name tiny_llama_2_bit_128g
     main()
