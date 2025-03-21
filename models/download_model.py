688bc8ad99bd9e219ed725aafe61995c400eb4b2
Alex Coleman <111077126+alexncoleman@users.noreply.github.com>
2025-03-21 11:16:36 +0000

    Adding Quantised model prompt outputs

--- models/download_model.py
+++ models/download_model.py
@@ -1,18 +1,22 @@
 from huggingface_hub import snapshot_download
 
 # Model repo on Hugging Face
-model_name = "BrownianNotion/TinyLlama_v1.1_mix_wikitext_alpaca_2bit_BitDistiller_baseline"
+# model_name = "BrownianNotion/TinyLlama_v1.1_mix_wikitext_alpaca_2bit_BitDistiller_baseline"
 
 # Download the entire model into the current directory
-#snapshot_download(repo_id=model_name, local_dir="models/TinyLlama_v1.1_mix_wikitext_alpaca_2bit_BitDistiller_baseline")
+# snapshot_download(repo_id=model_name, local_dir="models/TinyLlama_v1.1_mix_wikitext_alpaca_2bit_BitDistiller_baseline")
 
-print(f"Model {model_name} downloaded to TinyLlama_v1.1_mix_wikitext_alpaca_2bit_BitDistiller_baseline")
+# print(f"Model {model_name} downloaded to TinyLlama_v1.1_mix_wikitext_alpaca_2bit_BitDistiller_baseline")
 
 
-# Download 1 bit model as well
+# Download 1-bit model
 model_name = "fredericowieser/TinyLlama_v1.1_mix_wikitext_alpaca_1bit_BitDistiller_baseline"
 
-# Download the entire model into the current directory
-snapshot_download(repo_id=model_name, local_dir="models/TinyLlama_v1.1_mix_wikitext_alpaca_1bit_BitDistiller_baseline")
+# Download the entire model except 'globalstep_400' folder and the large file
+snapshot_download(
+    repo_id=model_name, 
+    local_dir="models/TinyLlama_v1.1_mix_wikitext_alpaca_1bit_BitDistiller_baseline",
+    ignore_patterns=["checkpoint-400/globalstep_400/*", "checkpoint-400/globalstep_400/bf16_zero_pp_rank_0_mp_rank_00_optim_states.pt"]
+)
 
-print(f"Model {model_name} downloaded to TinyLlama_v1.1_mix_wikitext_alpaca_2bit_BitDistiller_baseline")
+print(f"Model {model_name} downloaded to models/TinyLlama_v1.1_mix_wikitext_alpaca_1bit_BitDistiller_baseline")
