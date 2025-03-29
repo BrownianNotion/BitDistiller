148b06d3423a6adc33da346d1c58138227e1da97
Heisenger <julian.ma.24@ucl.ac.uk>
2025-03-29 15:39:43 +0000

    update download script to download different teacher adn student model

--- models/download_model.py
+++ models/download_model.py
@@ -1,9 +1,21 @@
 from huggingface_hub import snapshot_download
 
+
+# Download student model
 # Model repo on Hugging Face
 model_name = "meta-llama/Llama-3.2-3B"
+local_dir="models/Llama-3.2-3B"
 
 # Download the entire model into the current directory
-snapshot_download(repo_id=model_name, local_dir="models/Llama-3.2-3B")
+snapshot_download(repo_id=model_name, local_dir=local_dir)
+print(f"Model {model_name} downloaded to {local_dir}")
 
-print(f"Model {model_name} downloaded to models/Llama-3.2-3B")
+# Download teacher model
+# Model repo on Hugging Face
+model_name = "meta-llama/Llama-2-7b-hf"
+local_dir="models/Llama-2-7b-hf"
+
+# Download the entire model into the current directory
+snapshot_download(repo_id=model_name, local_dir=local_dir)
+
+print(f"Model {model_name} downloaded to {local_dir}")
